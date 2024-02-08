d2c3d2a84aec63e213124eca8923327d1e81f5fa
Kfir Hashay <kfirhashay@outlook.com>
2024-02-08 22:56:05 +0200

    Final Trello Clone

---
+++ config/site.ts
@@ -0,0 +1,7 @@
+const siteConfing = {
+  name: 'Taskify',
+  description:
+    'Collaborate, manage projects, and reach new productivity peaks',
+};
+
+export default siteConfing;
---
+++ prisma/migrations/20240208203923_init/migration.sql
@@ -0,0 +1,86 @@
+-- CreateTable
+CREATE TABLE `Board` (
+    `id` VARCHAR(191) NOT NULL,
+    `orgId` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `imageId` VARCHAR(191) NOT NULL,
+    `imageThumbUrl` TEXT NOT NULL,
+    `imageFullUrl` TEXT NOT NULL,
+    `imageUserName` TEXT NOT NULL,
+    `imageLinkHTML` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `List` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL,
+    `boardId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `List_boardId_idx`(`boardId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Card` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL,
+    `description` TEXT NULL,
+    `listId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Card_listId_idx`(`listId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AuditLog` (
+    `id` VARCHAR(191) NOT NULL,
+    `orgId` VARCHAR(191) NOT NULL,
+    `action` ENUM('CREATE', 'UPDATE', 'DELETE') NOT NULL,
+    `entityId` VARCHAR(191) NOT NULL,
+    `entityType` ENUM('BOARD', 'LIST', 'CARD') NOT NULL,
+    `entityTitle` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `userImage` TEXT NOT NULL,
+    `userName` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `OrgLimit` (
+    `id` VARCHAR(191) NOT NULL,
+    `orgId` VARCHAR(191) NOT NULL,
+    `count` INTEGER NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `OrgLimit_orgId_key`(`orgId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `OrgSubscription` (
+    `id` VARCHAR(191) NOT NULL,
+    `orgId` VARCHAR(191) NOT NULL,
+    `stripe_customer_id` VARCHAR(191) NULL,
+    `stripe_subscription_id` VARCHAR(191) NULL,
+    `stripe_price_id` VARCHAR(191) NULL,
+    `stripe_current_period_end` DATETIME(3) NULL,
+
+    UNIQUE INDEX `OrgSubscription_orgId_key`(`orgId`),
+    UNIQUE INDEX `OrgSubscription_stripe_customer_id_key`(`stripe_customer_id`),
+    UNIQUE INDEX `OrgSubscription_stripe_subscription_id_key`(`stripe_subscription_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
